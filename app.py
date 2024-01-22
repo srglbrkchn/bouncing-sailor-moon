45b36270db07b84ed1d09d578c752e7ea85b429e
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-01-22 13:40:54 +0100

    finish image movement on both axis. add background.

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 from tkinter import *
 import time
-WIDTH = 800
-HEIGHT = 500
-xSpeed = 1
-ySpeed = 1
+WIDTH = 700
+HEIGHT = 400
+xSpeed = 4
+ySpeed = 3
 
 window = Tk()
 canvas = Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
+backgroundimg = PhotoImage(file="sailor-moon-bg.png")
+lilbackground = backgroundimg.subsample(2, 2)
+background = canvas.create_image(0, 0, image=lilbackground, anchor=NW)
+
 myimage = PhotoImage(file="sailor-moon.png")
-lilimage = myimage.subsample(8, 8)
+lilimage = myimage.subsample(5, 5)
 moonimage = canvas.create_image(0, 0, image=lilimage, anchor=NW)
+
+
 
 while True:
     coordinates = canvas.coords(moonimage)
     print(coordinates)
     if(coordinates[0]>=(WIDTH - lilimage.width()) or coordinates[0]<0):
         xSpeed = xSpeed * -1
-    canvas.move(moonimage, xSpeed, 0)
+    if (coordinates[1] >= (HEIGHT - lilimage.height())or coordinates[1]<0):
+        ySpeed = ySpeed * -1
+    canvas.move(moonimage, xSpeed, ySpeed)
     window.update()
     time.sleep(0.01)
 window.mainloop()
